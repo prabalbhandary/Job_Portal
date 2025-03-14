e101776d588e8efff898c45187d9f09c5a25e193
Prabal Bhandary <109888062+prabalbhandary@users.noreply.github.com>
2025-03-14 08:48:13 +0545

    Update Footer.jsx

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
-  const getnewDate = new Date().getFullYear()
+  const getnewDate = new Date().getFullYear();
   return (
     <footer className="border-t border-t-gray-200 py-8">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <h2 className="text-xl font-bold">Job Hunt</h2>
-            <p className="text-sm">Copyright &copy; {getNewDate}. All rights reserved.</p>
+            <p className="text-sm">Copyright &copy; {getnewDate}. All rights reserved.</p>
           </div>
           
           <div className="flex space-x-4 mt-4 md:mt-0">
