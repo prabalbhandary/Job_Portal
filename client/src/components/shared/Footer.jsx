c5d6412d84103dc56afa010b9f737251356d8538
Prabal Bhandary <109888062+prabalbhandary@users.noreply.github.com>
2025-03-14 08:48:51 +0545

    Update Footer.jsx

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <h2 className="text-xl font-bold">Job Hunt</h2>
-            <p className="text-sm">Copyright &copy; {getnewDate}. All rights reserved.</p>
+            <p className="text-sm">Copyright &copy; {getnewDate} JobHunt. All rights reserved.</p>
           </div>
           
           <div className="flex space-x-4 mt-4 md:mt-0">
